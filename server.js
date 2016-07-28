63d64fdfce3c53909c2659cb2bb9d117d2fb653b
Michael Fleming <flemingatx@gmail.com>
2016-07-27 19:17:24 -0500

    added gitignore, package, server

---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var GithubStrategy = require('passport-github').Strategy;
+
+passport.use(new GithubStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // placeholder for translating profile into our own custom user object.
+    // for now we will just use the profile object returned by GitHub
+    return done(null, profile);
+  }
+));
+
+// Express and Passport Session
+var session = require('express-session');
+app.use(session({
+  secret: "enter custom sessions secret here",
+  resave: true,
+  saveUninitialized: true
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  // placeholder for custom user serialization
+  // null is for errors
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  // placeholder for custom user deserialization.
+  // null is for errors
+  done(null, user);
+});
+
+// we will call this to start the GitHub Login process
+app.get('/auth/github', passport.authenticate('github'));
+
+// GitHub will call this URL after line 37 runs
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/', function (req, res) {
+  var html = "<ul>\
+    <li><a href='/auth/github'>GitHub</a></li>\
+    <li><a href='/logout'>logout</a></li>\
+  </ul>";
+
+  // dump the user for debugging
+  if (req.isAuthenticated()) {
+    html += "<p>authenticated as user:</p>"
+    html += "<pre>" + JSON.stringify(req.user, null, 4) + "</pre>";
+  }
+
+  res.send(html);
+});
+
+app.get('/logout', function(req, res){
+  console.log('logging out');
+  req.logout();
+  res.redirect('/');
+});
+
+// Simple route middleware to ensure user is authenticated.
+//  Use this route middleware on any resource that needs to be protected.  If
+//  the request is authenticated (typically via a persistent login session),
+//  the request will proceed.  Otherwise, the user will be redirected to the
+//  login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/')
+}
+
+app.get('/protected', ensureAuthenticated, function(req, res) {
+  res.send("acess granted");
+});
+
+
+
+var server = app.listen(8080, function () {
+  console.log('Overlord listening on localhost:', server.address().port);
+});
