5f721956c0e64461b7568b155505715b7bc09cb4
Michael Fleming <flemingatx@gmail.com>
2016-07-29 15:44:20 -0500

    modified test db

---
+++ src/Server/fakedata.js
@@ -0,0 +1,19 @@
+let data = [];
+let db = {};
+let idCount = 0;
+
+db.addUser = function(user){
+	data.push({
+		id: idCount,
+		handle: user.handle,
+		image: user.image,
+		company: user.company
+	})
+	idCount++
+}
+
+db.getUser = function(){
+	return [data.userId, data.user, data.company]
+}
+
+module.exports = db;
