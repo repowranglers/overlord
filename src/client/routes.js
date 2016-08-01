e8f058933a67ea22215d8670a1bc5680b1840028
Travers Pinkerton <jtpinkerton@gmail.com>
2016-08-01 14:43:37 -0500

    add route for /dashboard

--- src/client/routes.js
+++ src/client/routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import Landing from './components/landing';
+import Dashboard from './components/dashboard';
 
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={Landing} />
-
+		<Route path="/dashboard" component={Dashboard} />
 	</Route>
 );
