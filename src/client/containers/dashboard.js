e3dac9e78975fbc5a449dfdd10c1a15197d52967
Timothy Crane <mickeymouth82@gmail.com>
2016-08-01 15:09:28 -0500

    create dashboard, resource form, project form

--- src/client/containers/dashboard.js
+++ src/client/containers/dashboard.js
@@ -3,13 +3,50 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
 import Modal from 'react-modal';
+import Projects from './projects';
+import Resources from './resources';
+import ProjectCreate from '../components/create_project';
+import ResourceCreate from '../components/create_resource';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : '30%',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { dashboardModal:  }
+    this.state = { CreateResourceModal: false };
+    this.state = { CreateProjectModal: false }
 
+    this.showCreateResourceModal = this.showCreateResourceModal.bind(this);
+    this.hideCreateResourceModal = this.hideCreateResourceModal.bind(this);
+    this.showCreateProjectModal = this.showCreateProjectModal.bind(this);
+    this.hideCreateProjectModal = this.hideCreateProjectModal.bind(this);
+
+  }
+
+  showCreateResourceModal() {
+    this.setState({ CreateResourceModal: true });
+  }
+
+  hideCreateResourceModal() {
+    this.setState({ CreateResourceModal: false });
+  }
+
+  showCreateProjectModal() {
+    this.setState({ CreateProjectModal: true });
+  }
+
+  hideCreateProjectModal() {
+    this.setState({ CreateProjectModal: false });
   }
 
   render() {
@@ -18,26 +55,33 @@ class Dashboard extends Component {
         <nav className="navbar">
             <ul className="navbar-list">
               <li className="navbar-item navbar-header">Overlord</li>
-              <button className="create-resource">Create Resource</button>
-              <button className="create-project">Create Project</button>
+              <button onClick={this.showCreateResourceModal.bind(this)}className="create-resource">Create Resource</button>
+              <button onClick={this.showCreateProjectModal.bind(this)}className="create-project">Create Project</button>
             </ul>
         </nav>
 
+        <Modal
+          isOpen={this.state.CreateResourceModal}
+          onRequestClose={this.hideCreateResourceModal}
+          style={customStyles}
+        ><ResourceCreate />
+        </Modal>
+        <Modal
+          isOpen={this.state.CreateProjectModal}
+          onRequestClose={this.hideCreateProjectModal}
+          style={customStyles}
+        >
+        <ProjectCreate />
+        </Modal>
+
         <h4 id="projects-header">Projects</h4>
-        <Modal >
-
+          <Projects />
+        
         <h4 id="resources-header">Resources</h4>
-        <Modal >
-
+          <Resources />
       </div>
     )
   }
-}
-
-function mapStateToProps(state) {
-  return {
-    projects: state.projects
-  };
 }
 
 export default Dashboard;
