8bbbeb78a1ca708b6ea64335e833cc3c10a1dae8
Travers Pinkerton <jtpinkerton@gmail.com>
2016-08-02 13:49:37 -0500

    update components to better display dashboard and projects

--- src/client/containers/projects.js
+++ src/client/containers/projects.js
@@ -3,18 +3,18 @@ import { connect } from 'react-redux';
 import { fetchProjects } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
-
-let id = 0;
-
 class Projects extends Component {
-
   render() {
     return (
       <div>
       { this.props.projectList[0] ? this.props.projectList[0].map( project => {
-        id++;
         return (
-          <p key={id} className="list-group-item">{project.proj_name}</p>
+          <ul key={project.proj_name} className="list-group">
+            <h5>{project.proj_name}</h5>
+            <li className="list-group-item">{project.start}</li>
+            <li className="list-group-item">{project.due}</li>
+            <li className="list-group-item">{project.status}</li>
+          </ul>
         );
       } ) : null }
 
--- src/client/containers/resources.js
+++ src/client/containers/resources.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchProjects } from '../actions/index';
 
-
 class Resources extends Component {
-  renderList() {
-    return this.props.resources.map((resource) => {
-      return (
-        <li key={resource.name} className="list-group-item">{resource.name}</li>
-      );
-    });
-  }
-
   render() {
     return (
-      <ul className="list-group">
-        {this.renderList()}
-      </ul>
+      <div>
+      { this.props.resourceList[0] ? this.props.resourceList[0].map( resource => {
+        return (
+          <ul key={resource.proj_name} className="list-group">
+            <h5>{resource.res_name}</h5>
+            <li className="list-group-item">{resource.start}</li>
+            <li className="list-group-item">{resource.due}</li>
+            <li className="list-group-item">{resource.status}</li>
+          </ul>
+        );
+      } ) : null }
+
+      </div>
     )
   }
 } 
 
-function mapStateToProps(state) {
-  return {
-    resources: state.resources
-  };
-}
 
 export default connect(mapStateToProps, { fetchProjects })(Resources);
