11b945b50e639fd13c7f6a45373884cb23efd4c5
Travers Pinkerton <jtpinkerton@gmail.com>
2016-08-02 16:13:48 -0500

    update reducers to proper state

--- src/client/reducers/reducer_resources.js
+++ src/client/reducers/reducer_resources.js
@@ -1,9 +1,10 @@
-export default function () {
-  return [
-    { resource: 'Mike Fleming'},
-    { resource: 'Travers Pinkerton'},
-    { resource: 'Charlie Carrigan'},
-    { resource: 'Dillon'},
-    { resource: 'Tim Crane'}
-  ]
+import { FETCH_RESOURCES } from '../actions/index';
+
+export default function(state = [], action) {
+	switch(action.type) {
+		case FETCH_RESOURCES:
+			console.log('reducer_resources action.payload.data ', action.payload.data)
+			return [ action.payload.data, ...state ];
+	}
+	return state;
 }
