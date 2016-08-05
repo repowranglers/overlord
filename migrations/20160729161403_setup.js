665600ce59095d916164851a61bf6afffb7176c1
Michael Fleming <flemingatx@gmail.com>
2016-08-05 12:13:25 -0500

    adds a description column to projects, updates tests

--- migrations/20160729161403_setup.js
+++ migrations/20160729161403_setup.js
@@ -14,6 +14,7 @@ exports.up = function(knex, Promise) {
   		table.date('start');
   		table.date('due');
   		table.string('status');
+      table.string('description');
   	}),
   	knex.schema.createTable('resources', function(table){
   		table.increments('res_id').primary();
