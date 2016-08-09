76617a08ad499c7bbef653b95ad2ffda03423951
Dillon Lundell <dillonjaylundell@gmail.com>
2016-08-09 18:01:23 -0500

    projects contain current draggable resources

--- src/client/containers/resource_project_view.js
+++ src/client/containers/resource_project_view.js
@@ -67,7 +67,7 @@ class ResourceView extends Component {
       <h3 className="title">Projects</h3>
 
       { this.props.projectList[0] ? this.props.projectList[0].map( project => {
-        console.log('projects', project)
+        console.log('projects', project.resources)
         
         return (
           <div key={project.proj_name}>
@@ -77,12 +77,18 @@ class ResourceView extends Component {
   
             <button className="button story-create" onClick={() => this.showCreateStoryModal(project.project_id)}>Create Story</button>
     
- <button className="delete-btn" onClick={() => this.onDelete(project.project_id)}>Delete</button>
+              <button className="delete-btn" onClick={() => this.onDelete(project.project_id)}>Delete</button>
 
             <div>
             <p>Resources on Projects</p>
             <div id={project.project_id} key={project.project_id} className='right container' ref={this.dragulaDecorator}>
                 <div className="item"> Drag resources here </div>
+                 { project.resources ? project.resources.filter(r => r.res_name !== '').map( r => {
+            return (
+            <div id={r.res_id} className="item image-thing" key={r.res_name}><img src= {`/images/${r.res_img}`}></img> <br/> {r.res_name}
+            <button className="delete-btn" onClick={() => this.onDelete(r.res_id)}>Delete</button></div>
+          );
+        } ) : null }
             </div>
             </div>
           </div>
