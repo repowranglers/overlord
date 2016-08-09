9713579bd4fe64b85090e30dc0f740f6895a328e
Michael Fleming <flemingatx@gmail.com>
2016-08-09 15:46:57 -0500

    minor styling tweaks

--- src/client/components/landing.js
+++ src/client/components/landing.js
@@ -36,25 +36,27 @@ class Landing extends Component {
 
   render() {
     return (
-      <div className="background flashit">
-        <div className="rain">
-          <nav className="navbar">
-              <ul className="navbar-list">
-                <li className="navbar-item navbar-header">Overlord</li>
-                  <a className="button button-primary" href='/auth/github'>Login with GitHub</a> 
-              </ul>
-          </nav>
+      <div>
+        <nav className="navbar">
+            <ul className="navbar-list">
+              <li className="navbar-item navbar-header">Overlord</li>
+                <a className="button button-primary" href='/auth/github'>Login with GitHub</a> 
+            </ul>
+        </nav>
+        <div className="background flashit">
+          <div className="rain">
 
-          <h2 className="top-header">Streamline Your Project Management</h2>
+            <h2 className="top-header">Streamline Your Project Management</h2>
 
-          <Modal
-            isOpen={this.state.signInModal}
-            onRequestClose={this.hideSignInModal}
-            style={customStyles}>
-            <div>
-            <button className="button-primary" onClick={this.props.githubLogin.bind(this)}>Log in with Github</button>
-            </div>
-          </Modal>
+            <Modal
+              isOpen={this.state.signInModal}
+              onRequestClose={this.hideSignInModal}
+              style={customStyles}>
+              <div>
+              <button className="button-primary" onClick={this.props.githubLogin.bind(this)}>Log in with Github</button>
+              </div>
+            </Modal>
+          </div>
         </div>
       </div>
     );
