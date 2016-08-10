5d8fb784d5a5874e677b972b12bcff58593b1caf
Charlie Carrigan <ctcarrigan33@gmail.com>
2016-08-10 11:50:31 -0500

    creating Charts component

---
+++ src/client/components/create_charts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ChartCreate extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+
+    );
+  }
+
+}
