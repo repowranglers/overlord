69fba65fbadc26d27c0143be7b9837fbf1e838be
Charlie Carrigan <ctcarrigan33@gmail.com>
2016-08-16 14:23:27 -0500

    removing console logs

--- src/client/components/charts.js
+++ src/client/components/charts.js
@@ -38,11 +38,6 @@ export default class Charts extends Component {
 
             data = {this.props.projectList
                    ? this.props.projectList.map(val => {
-                     console.log("VAL ", val)
-                     console.log("val.proj_name ", val.proj_name)
-                     console.log('# of PROJ RESOURCES: ', val.resources.length)
-                     console.log('TOTAL # of RESOURCES: ', this.props.resourceList.length)
-                     console.log('Y VALUE: ', val.resources.length / this.props.resourceList.length)
                      return {
                        x: val.proj_name,
                        y: (val.resources.length / this.props.resourceList.length)
