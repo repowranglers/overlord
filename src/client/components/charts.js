e0825e6050cb6c2a9c0e9b1df69be61d92982b50
Charlie Carrigan <ctcarrigan33@gmail.com>
2016-08-16 11:19:27 -0500

    rolling back pie chart

--- src/client/components/charts.js
+++ src/client/components/charts.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { render } from 'react-dom';
-import { VictoryPie } from 'victory';
+import { VictoryPie, VictoryStack, VictoryBar } from 'victory';
 
 export default class Charts extends Component {
   constructor(props) {
@@ -11,37 +11,124 @@ export default class Charts extends Component {
   }
 
 
+    // you can use functional styles for that too. Maybe something like scaling datum.y by some appropriate value to set fontSize
+
   render() {
     return (
-      <div className="pie-chart">Resources per Project
+      <div>
+        <div className="pie-chart">
+          <h2 className="pie-chart-header">Resources per Project</h2>
+          <VictoryPie
+            height={250}
+            width={250}
 
-        <VictoryPie
-          height={250}
-          width={250}
+            style={{
+              data: {
+                stroke: (datum) => datum.y > 75 ?
+                  "tomato" : "black",
+                strokeWidth: (datum) => datum.y > 75 ?
+                  3 : 1
+              },
+              labels: {
+                fill: "white",
+                fontSize: 10,
+                angle: (datum) => datum.y > 75 ? 0 : 45
+              }
+            }}
 
-          style={{
-            data: {
-              stroke: (data) => data.y > 0.5 ?
-                "tomato" : "blue"
-            },
-            labels: {
-              fill: "red",
-              fontSize: 10
-            }
-          }}
-          data = {this.props.projectList
-                  ? this.props.projectList.map(val => {
-                    console.log("VAL ", val)
-                    console.log("val.proj_name ", val.proj_name)
-                    console.log('# of PROJ RESOURCES: ', val.resources.length)
-                    console.log('TOTAL # of RESOURCES: ', this.props.resourceList.length)
-                    console.log('Y VALUE: ', val.resources.length / this.props.resourceList.length)
-                    return {
-                      x: val.proj_name,
-                      y: (val.resources.length / this.props.resourceList.length)
-                    }
-        }):[]}
-        />
+            data = {this.props.projectList
+                   ? this.props.projectList.map(val => {
+                     console.log("VAL ", val)
+                     console.log("val.proj_name ", val.proj_name)
+                     console.log('# of PROJ RESOURCES: ', val.resources.length)
+                     console.log('TOTAL # of RESOURCES: ', this.props.resourceList.length)
+                     console.log('Y VALUE: ', val.resources.length / this.props.resourceList.length)
+                     return {
+                       x: val.proj_name,
+                       y: (val.resources.length / this.props.resourceList.length)
+                     }
+            }):[]}
+
+            // data = {(!this.props.projectList ||     !this.props.resourcesList.length)
+            //   ? null
+            //   : return (
+            //   this.props.projectList
+            //           ? this.props.projectList.map(val => {
+            //             console.log("VAL ", val)
+            //             console.log("val.proj_name ", val.proj_name)
+            //             console.log('# of PROJ RESOURCES: ', val.resources.length)
+            //             console.log('TOTAL # of RESOURCES: ', this.props.resourceList.length)
+            //             console.log('Y VALUE: ', val.resources.length / this.props.resourceList.length)
+            //             return {
+            //               x: val.proj_name,
+            //               y: (val.resources.length / this.props.resourceList.length)
+            //             }
+            //   }):[]
+            // )}
+
+            //animate={{
+            //  duration: 1000,
+            //  onEnter: {
+            //    duration: 500,
+            //    before: () =>
+            //      ({y: 0, label: " "}),
+            //    after: (datum) =>
+            //      ({y: datum.y, label: "NEW"})
+            //  }
+            //}}
+
+            colorScale={[
+              "#ff0000",
+              "#cc0000",
+              "#b20000",
+              "#7f0000",
+              "#4c0000",
+              "#330000",
+              "#000000"
+            ]}
+          />
+        </div>
+
+        {/*<div className="horizontal-stack">
+          <h2 className="horizontal-stack-header">LIFE</h2>
+          <VictoryStack horizontal
+            height={250}
+            padding={75}
+            style={{
+              data: {width: 30},
+              labels: {
+                fontSize: 24,
+                fill: "white"
+              }
+            }}
+            labels={["one", "two", "three"]}
+          >
+            <VictoryBar
+              style={{data: {fill: "tomato"}}}
+              data={[
+                {x: 1, y: 1},
+                {x: 2, y: 2},
+                {x: 3, y: 3}
+              ]}
+            />
+            <VictoryBar
+              style={{data: {fill: "orange"}}}
+              data={[
+                {x: 1, y: 2},
+                {x: 2, y: 1},
+                {x: 3, y: 1}
+              ]}
+            />
+            <VictoryBar
+              style={{data: {fill: "gold"}}}
+              data={[
+                {x: 1, y: 3},
+                {x: 2, y: 4},
+                {x: 3, y: 2}
+              ]}
+            />
+          </VictoryStack>
+        </div>*/}
       </div>
     );
   }
@@ -49,3 +136,11 @@ export default class Charts extends Component {
 }
 //y: # res on proj/total res
 //x: proj name
+
+
+
+// animate Charts
+// debug pie Chart
+// move/adapt bar Chart
+  // - project view
+  // - number of user stories completed vs incomplete
